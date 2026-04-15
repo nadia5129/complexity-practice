376419b58da01dec53a0fe1ca0cf784d2883f839
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-04-14 20:26:11 -0700

    completed last three questions

--- src/Main.java
+++ src/Main.java
@@ -214,14 +214,14 @@ public class Main {
   // The prices will be updated frequently throughout the day, and you need to efficiently update
   // and access the current price for each stock. The order of the ticker symbols is not important.
   // What would be a good choice of data structure?
-  // YOUR ANSWER HERE
+  // YOUR ANSWER HERE: hashMap because it has keys
 
   // Suppose you are building a music player application where users can create playlists.
   // Songs can be added to the end of the playlist in the order the user chooses, and the user can
   // skip to the next or previous song. Most operations involve adding songs and accessing them by
   // their position in the playlist.
   // What would be a good choice of data structure?
-  // YOUR ANSWER HERE
+  // YOUR ANSWER HERE: ArrayList because you can add to the end easily
 
   // Suppose you are developing a search feature that keeps track of the user's
   // recent search queries. You want to store the queries in the order they were made,
@@ -229,5 +229,5 @@ public class Main {
   // relatively small, and it is more important to preserve the order of the searches than
   // to optimize for fast lookups or deletions.
   // What would be a good choice of data structure?
-  // YOUR ANSWER HERE
+  // YOUR ANSWER HERE: arrayList
 }
