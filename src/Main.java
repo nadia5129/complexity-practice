588cd480778a7dcfadc51d52b637a4e2da485afb
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-04-14 20:01:46 -0700

    completed question 3 and 4

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
   }
 
   // The time complexity is:
-  // YOUR ANSWER HERE
+  // YOUR ANSWER HERE: o(1)
   public static boolean isBanned(String password) {
     String[] bannedPasswords = {"password", "hello", "qwerty"};
     boolean banned = false;
@@ -41,7 +41,7 @@ public class Main {
 
 
   // The time complexity is:
-  // YOUR ANSWER HERE
+  // YOUR ANSWER HERE: o(n)
   public static int computeProduct(int[] nums) {
     int total = 1;
     for(int num : nums) {
